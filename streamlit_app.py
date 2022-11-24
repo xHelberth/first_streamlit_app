1108938f69096aac56e7f0d5fcfe31f3e378e456
xHelberth <106198990+xHelberth@users.noreply.github.com>
2022-11-24 12:51:16 -0600

    Update streamlit_app.py
    Add New header for Fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
